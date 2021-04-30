620c70d184684408705b0d5ac1acfe7e01a5577e
zqs <zhuqingsong@weihaizixun.com>
2021-04-30 17:32:06 +0800

    有问题，待处理

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,9 @@
+// 帮助寻找node_modules里的包
+import resolve from 'rollup-plugin-node-resolve'
+// 将非ES6语法的包转为ES6可用
+import commonjs from 'rollup-plugin-commonjs'
+// rollup 的 babel 插件，ES6转ES5
+import babel from 'rollup-plugin-babel'
 // 允许 Rollup 从 JSON 文件中导入数据
 import json from 'rollup-plugin-json'
 
@@ -8,5 +14,16 @@ export default {
     format: 'umd',
     name: 'jsEasy'
   },
-  plugins: [ json() ]
+  watch: {
+    exclude: 'node_modules/**'
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    json(),
+    babel({
+      exclude: '**/node_modules/**',
+      plugins: ['external-helpers']
+    })
+  ]
 }
